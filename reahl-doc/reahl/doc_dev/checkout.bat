692bbf7806f3ff578324cbe728bb241d2434d898
craig@reahl.org <>
2014-04-03 08:42:59 +0000

    removed rem comments not needed

--- reahl-doc/reahl/doc_dev/checkout.bat
+++ reahl-doc/reahl/doc_dev/checkout.bat
@@ -17,7 +17,7 @@ for /F %%i in %EXAMPLE_LIST% do (
             echo ---Example [%%i] is in a directory: !EXAMPLE_DIR_OR_FILE!
             cd !EXAMPLE_DIR_OR_FILE!
             reahl setup -- develop -N
-            REM reahl unit
+            reahl unit
             reahl setup -- develop -N --uninstall
             cd %REAHL_TMP%
         ) else (
@@ -30,43 +30,3 @@ ENDLOCAL
 cd %ORIGINAL_DIR%
 rmdir /S /Q %REAHL_TMP%
 
-   
-REM do
-    REM result="E"
-    REM out=$(cd /tmp; reahl example -f $i | awk '/Checking out to/ {print $4}')
-    REM if [ -f $out ]
-    REM then
-        REM if nosetests $out
-        REM then 
-            REM result="."
-        REM fi
-    REM else
-        REM cd $out
-        REM reahl setup -- develop -N
-        REM if reahl unit 
-        REM then
-            REM result="."
-        REM fi
-        REM reahl setup -- develop -N --uninstall
-        REM cd -
-    REM fi
-    REM results["$i"]=$result
-    REM rm -rf $out
-REM done
-
-REM cd /tmp; reahl example -f tutorial.migrationexample
-REM cd /tmp/migrationexample
-REM chmod u+x /tmp/migrationexample/migrationexample_dev/test.sh
-REM if /tmp/migrationexample/migrationexample_dev/test.sh
-REM then 
-    REM results["migrationexample (actual migrating)"]="."
-REM else
-    REM results["migrationexample (actual migrating)"]="E"
-REM fi
-REM cd -
-REM rm -rf /tmp/migrationexample
-
-REM for x in "${!results[@]}"
-REM do
-    REM echo ${results["$x"]} $x
-REM done
