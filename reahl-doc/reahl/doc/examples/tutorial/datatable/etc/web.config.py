353892bdc81393e9e64ab53994d15be6dc7f303a
craig@reahl.org <>
2014-08-07 09:09:05 +0000

    examples should have the full path, will be chnaged when checked out

--- reahl-doc/reahl/doc/examples/tutorial/datatable/etc/web.config.py
+++ reahl-doc/reahl/doc/examples/tutorial/datatable/etc/web.config.py
@@ -2,6 +2,6 @@
 
 from __future__ import unicode_literals
 from __future__ import print_function
-from datatable import AddressBookUI
+from reahl.doc.examples.tutorial.datatable.datatable import AddressBookUI
 
 web.site_root = AddressBookUI
