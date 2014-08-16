75ec62acef453afda4169905149e344aa1dac524
Pieter Nagel <pieter@nagel.co.za>
2014-08-16 11:40:12 +0000

    Stubble: deahl with types/classes printing themselves differently under Py3 vs Py2

--- reahl-stubble/reahl/stubble_dev/BasicStubRequirementsTests.py
+++ reahl-stubble/reahl/stubble_dev/BasicStubRequirementsTests.py
@@ -16,9 +16,8 @@
 
 from __future__ import unicode_literals
 from __future__ import print_function
-import six
 
-from nose.tools import istest, assert_raises, assert_true, assert_equals
+from nose.tools import istest, assert_raises, assert_raises_regexp, assert_true
 
 from reahl.stubble import stubclass, exempt, EmptyStub
 
@@ -50,148 +49,123 @@ class BasicStubRequirementsTests(object):
     @istest
     def test_method_attribute_mismatch(self):
         """a method in the stub which is an attribute in the stubbed is not allowed"""
-        def declare_class():
+        #normal case
+        with assert_raises_regexp(
+                AssertionError,
+                "^attribute mismatch: <class '.*\.Stub'>\.<(unbound method|function) .*Stub\.attr.*> is not compatible with the original type <(type|class) 'int'> on <class '.*Stubbed'>"):
             @stubclass(self.stubbed)
             class Stub(object):
-                def attr(self): pass
-        
-        #normal case
-        with assert_raises(AssertionError) as context:
-            declare_class()
-        assert_equals( six.text_type(context.exception), '''attribute mismatch: <class 'reahl.stubble_dev.BasicStubRequirementsTests.Stub'>.<unbound method Stub.attr> is not compatible with the original type <type 'int'> on <class 'reahl.stubble_dev.BasicStubRequirementsTests.Stubbed'>''' )
+                def attr(self):
+                    pass
 
     @istest
     def test_method_signature_mismatch(self):
         """a method signature mismatch between the stub and the stubbed is not allowed"""
-        def declare_class():
+        #normal case
+        with assert_raises_regexp(
+                AssertionError,
+                '^signature mismatch:.*Stub.method.*\(self, b, akwarg=None, \*args, \*\*kwargs\) does not match .*Stubbed.method.*\(self, a, b, akwarg=None, \*args, \*\*kwargs\)$'):
             @stubclass(self.stubbed)
             class Stub(object):
-                def method(self, b, akwarg=None, *args, **kwargs): pass
+                def method(self, b, akwarg=None, *args, **kwargs):
+                    pass
 
-        #normal case
-        with assert_raises(AssertionError) as context:
-            declare_class()
-        
-        assert_equals( six.text_type(context.exception), '''signature mismatch: <unbound method Stub.method>(self, b, akwarg=None, *args, **kwargs) does not match <unbound method Stubbed.method>(self, a, b, akwarg=None, *args, **kwargs)''' )
 
     @istest
     def test_property_method_missing_on_orig(self):
         """a property on the stub is not allowed if the stubbed class does not have property or an attribute"""
-        def declare_class():
+        #case where the property does ont exist on the stubbed class
+        with assert_raises(AssertionError):
             @stubclass(self.stubbed)
             class Stub(object):
                 @property
                 def non_existing_property(self):
                     return 2
 
-        #case where the property does ont exist on the stubbed class
-        assert_raises(AssertionError, declare_class)
-
     @istest
     def test_property_method_masks_method_on_orig(self):
         """a property on the stub is not allowed if the stubbed class has a method with the same name"""
-        def declare_class():
+        #case where the property does ont exist on the stubbed class
+        with assert_raises(AssertionError):
             @stubclass(self.stubbed)
             class Stub(object):
                 @property
                 def method(self):
                     return 2
 
-        #case where the property does ont exist on the stubbed class
-        assert_raises(AssertionError, declare_class)
-
     @istest
     def test_property_method_for_attribute_on_orig(self):
         """a property on the stub is allowed if the stubbed class has a property or an attribute of the same name"""
         #case where the stubbed class has an attribute for the property
-        def declare_class():
-            @stubclass(self.stubbed)
-            class Stub(object):
-                @property
-                def attr(self):
-                    return 2
-
-        declare_class()
+        @stubclass(self.stubbed)
+        class Stub(object):
+            @property
+            def attr(self):
+                return 2
 
         #case where the stubbed class has a matching property
-        def declare_class():
-            @stubclass(self.stubbed)
-            class Stub(object):
-                @property
-                def my_existing_property(self):
-                    return 2
-
-        declare_class()
+        @stubclass(self.stubbed)
+        class Stub(object):
+            @property
+            def my_existing_property(self):
+                return 2
 
     @istest
     def test_attribute_not_present_mismatch(self):
         """an attribute in the stub which is not in the stubbed is not allowed"""
-        def declare_class():
+        #normal case
+        with assert_raises(AssertionError):
             @stubclass(self.stubbed, True)
             class Stub(object):
                 i_am_not = 1
 
-        #normal case
-        assert_raises(AssertionError, declare_class)
-
     @istest
     def test_method_name_mismatch(self):
         """a method in the stub which has no counterpart in the stubbed is not allowed"""
-        def declare_class():
+        #normal case
+        with assert_raises(AssertionError):
             @stubclass(self.stubbed)
             class Stub(object):
-                def method1(self, a, b): pass
-
-        #normal case
-        assert_raises(AssertionError, declare_class)
+                def method1(self, a, b):
+                    pass
 
     @istest
     def test_normal_method(self):
         """a method in the stub which accurately describes one in the stubbed is allowed"""
-        def declare_class():
-            @stubclass(self.stubbed)
-            class Stub(object):
-                def method(self, a, b, akwarg=None, *args, **kwargs): pass
-
         #normal case
-        declare_class()
+        @stubclass(self.stubbed)
+        class Stub(object):
+            def method(self, a, b, akwarg=None, *args, **kwargs):
+                pass
+
 
     @istest
     def test_normal_attribute(self):
         """an attribute in the stub which is also present in the stubbed is allowed"""
 
         #normal case
-        def declare_class():
-            @stubclass(self.stubbed, True)
-            class Stub(object):
-                attr = 1
-
-        declare_class()
+        @stubclass(self.stubbed, True)
+        class Stub(object):
+            attr = 1
 
         #case where we're not checking for attributes
-        def declare_class():
-            @stubclass(self.stubbed)
-            class Stub(object):
-                i_am_not = 1
-
-        declare_class()
+        @stubclass(self.stubbed)
+        class Stub(object):
+            i_am_not = 1
 
     #----------------------------------------[ exempt members ]
     @istest
     def test_exempt_attribute(self):
         """an attribute or property marked exempt does not raise an error"""
-        def declare_class():
-            @stubclass(self.stubbed, True)
-            class Stub(object):
-                attr = 1
-                attr = exempt(attr)
-                @exempt
-                @property
-                def prop(self): pass
-            return Stub
-
+        @stubclass(self.stubbed, True)
         #normal case
-        Stub = declare_class()
+        class Stub(object):
+            attr = 1
+            attr = exempt(attr)
+            @exempt
+            @property
+            def prop(self):
+                pass
 
         # the property returns the correct value
         assert Stub().prop is None
@@ -199,59 +173,60 @@ class BasicStubRequirementsTests(object):
     @istest
     def test_exempt_method(self):
         """any methods marked as exempt does not raise an error"""
-        def declare_class():
-            @stubclass(self.stubbed)
-            class Stub(object):
-                @exempt
-                def local_method(self): pass
-
-                @exempt
-                def method(): pass
-
-                @exempt
-                @classmethod
-                def local_class_method(cls): pass
-
-                @exempt
-                @classmethod
-                def a_class_method(cls): pass
-            return Stub
-
         #normal case
-        _class = declare_class()
-        _class.local_class_method()
-        _class.a_class_method()
+        @stubclass(self.stubbed)
+        class Stub(object):
+            @exempt
+            def local_method(self):
+                pass
+
+            @exempt
+            def method():
+                pass
+
+            @exempt
+            @classmethod
+            def local_class_method(cls):
+                pass
+
+            @exempt
+            @classmethod
+            def a_class_method(cls):
+                pass
+
+        Stub.local_class_method()
+        Stub.a_class_method()
 
     @istest
     def test_exempt_inherited_method(self):
         """any methods marked as exempt does not raise an error, even if they are inherited by the stub"""
-        def declare_class():
-            @stubclass(self.stubbed)
-            class Stub(object):
-                @exempt
-                def local_method(self): pass
-
-                @exempt
-                def method(): pass
-
-                @exempt
-                @classmethod
-                def local_class_method(cls): pass
-
-                @exempt
-                @classmethod
-                def a_class_method(cls): pass
-
-
-            @stubclass(self.stubbed)
-            class InheritingStub(Stub):
-                pass
-            
-            return InheritingStub
-
         #normal case
-        _class = declare_class()
-        _class.local_class_method()
-        _class.a_class_method()
+        @stubclass(self.stubbed)
+        class Stub(object):
+            @exempt
+            def local_method(self):
+                pass
+
+            @exempt
+            def method(self):
+                pass
+
+            @exempt
+            @classmethod
+            def local_class_method(cls):
+                pass
+
+            @exempt
+            @classmethod
+            def a_class_method(cls):
+                pass
+
+
+        @stubclass(self.stubbed)
+        class InheritingStub(Stub):
+            pass
+
+        InheritingStub.local_class_method()
+        InheritingStub.a_class_method()
 
 
