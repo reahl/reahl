559aa0b59964693b2ca757536dfcee12d99f53e2
Pieter Nagel <pieter@nagel.co.za>
2014-08-18 07:46:19 +0000

    Fix MethodType instantiation in reahl component

--- reahl-component/reahl/component/modelinterface.py
+++ reahl-component/reahl/component/modelinterface.py
@@ -1130,7 +1130,7 @@ class SecuredDeclaration(object):
         return arg_spec.args[:positional_args_end]
 
     def __get__(self, instance, owner):
-        method = types.MethodType(self.func, instance, owner)
+        method = six.create_bound_method(self.func, instance)
         return SecuredMethod(method, self)
 
 
