c183a5d7f949bf8f14001a65b679c64b476d88a1
Pieter Nagel <pieter@nagel.co.za>
2014-08-30 07:52:05 +0000

    Fix broken ambiguous imports revealed by enabling absolute_import

--- reahl-domain/reahl/partymodel.py
+++ reahl-domain/reahl/partymodel.py
@@ -2,4 +2,4 @@ from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import division
 from __future__ import unicode_literals
-from domain.partymodel import *
+from .domain.partymodel import *
