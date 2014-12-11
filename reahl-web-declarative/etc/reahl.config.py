c04985cb863cf925318c977b38c5b77bb91dffb4
craig@reahl.org <>
2014-12-11 10:21:08 +0200

    added tox.ini for projects who do not have tests ; added missing dep ; reahl.config hardcoded root_egg

--- reahl-web-declarative/etc/reahl.config.py
+++ reahl-web-declarative/etc/reahl.config.py
@@ -1,32 +1,12 @@
-# Copyright 2013, 2014 Reahl Software Services (Pty) Ltd. All rights reserved.
-#
-#    This file is part of Reahl.
-#
-#    Reahl is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation; version 3 of the License.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 from __future__ import print_function, unicode_literals, absolute_import, division
-
-import os
-import os.path
 
 from reahl.sqlalchemysupport import SqlAlchemyControl
 
-eggName = os.path.basename(os.getcwd())
-reahlsystem.root_egg = eggName
+reahlsystem.root_egg = 'reahl-web-declarative'
 reahlsystem.connection_uri = 'postgresql://rhug:rhug@localhost/rhug'
 #reahlsystem.connection_uri = 'sqlite://'
 reahlsystem.orm_control = SqlAlchemyControl(echo=False)
-reahlsystem.debug = True
+#reahlsystem.debug = True
 
 
 
