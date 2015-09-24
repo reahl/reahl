c57d94e5dc02363a02e358201fb415ef24218e37
iwan <iwan@reahl.org>
2015-09-24 14:42:05 +0200

    finished changing bootstrap tests for alpha4

--- reahl-web/reahl/web_dev/bootstrap/components/navtests.py
+++ reahl-web/reahl/web_dev/bootstrap/components/navtests.py
@@ -25,32 +25,41 @@ from reahl.web_dev.fixtures import WebFixture
 
 from reahl.component.exceptions import ProgrammerError
 from reahl.web.fw import Bookmark, Url
-from reahl.web.ui import A
+from reahl.web.ui import A, Div
 
-from reahl.web.bootstrap.navs import Nav, MenuItem, PillLayout, TabLayout
+from reahl.web.bootstrap.navs import Nav, PillLayout, TabLayout, DropdownMenu, DropdownMenuLayout
+
 
 
 @test(WebFixture)
 def navs(fixture):
-    """A Nav displays a Menu with a link for each of its MenuItems"""
+    """A Nav is a Menu with css classes for styling by Bootstrap."""
 
     bookmarks = [Bookmark('', '/one', 'One'),
                  Bookmark('', '/two', 'Two')]
-    menu = Nav.from_bookmarks(fixture.view, bookmarks)
+    menu = Nav(fixture.view).with_bookmarks(bookmarks)
     
     # A nav is an ul.nav
-    vassert( menu.tag_name == 'ul' )
-    vassert( 'nav' in menu.get_attribute('class') )
+    vassert( menu.html_representation.tag_name == 'ul' )
+    vassert( 'nav' in menu.html_representation.get_attribute('class') )
 
     # Containing a li for each menu item
-    [one, two] = menu.children
+    [one, two] = menu.html_representation.children
 
     for item, expected_href, expected_description in [(one, '/one', 'One'),
                                                       (two, '/two', 'Two')]:
         vassert( item.tag_name == 'li' )
+        vassert( item.get_attribute('class') == 'nav-item' )
+
         [a] = item.children
         vassert( a.get_attribute('href') == expected_href )
         vassert( a.children[0].value ==  expected_description )
+        vassert( a.get_attribute('class') == 'nav-link' )
+
+# To test:
+#  That dropdown menus open/close when no javascript
+#  and that they do not actually break in js when JS is on.
+#  that Menu & Nav & DropdownMenu have default layouts if you never specify one
 
 
 class VisualFeedbackScenarios(WebFixture):
@@ -59,14 +68,14 @@ class VisualFeedbackScenarios(WebFixture):
         """The mouse cursor is shown as no-access on disabled items."""
         def not_allowed():
             return False
-        self.menu_item_with_state = MenuItem(self.view, A(self.view, Url('/another_url'), write_check=not_allowed))
+        self.menu_item_with_state = A(self.view, Url('/another_url'), write_check=not_allowed)
         self.state_indicator_class = 'disabled'
 
     @scenario
     def active(self):
         """The currently active item is highlighted."""
         current_url = Url(self.request.url)
-        self.menu_item_with_state = MenuItem(self.view, A(self.view, current_url))
+        self.menu_item_with_state = A(self.view, current_url)
         self.state_indicator_class = 'active'
 
 
@@ -74,13 +83,16 @@ class VisualFeedbackScenarios(WebFixture):
 def visual_feedback_on_items(fixture):
     """The state of a MenuItem is visually indicated to a user."""
     menu = Nav(fixture.view, [])
-    menu.add_item(MenuItem(fixture.view, A(fixture.view, Url('/an_url'))))
-    menu.add_item(fixture.menu_item_with_state)
+    menu.add_a(A(fixture.view, Url('/an_url')))
+    menu.add_a(fixture.menu_item_with_state)
 
-    [defaulted_item, item_with_state] = menu.children
+    [defaulted_item, item_with_state] = menu.html_representation.children
 
-    vassert( not defaulted_item.has_attribute('class') )
-    vassert( fixture.state_indicator_class in item_with_state.get_attribute('class') )
+    [defaulted_a] = defaulted_item.children
+    [a_with_state] = item_with_state.children
+    
+    vassert( fixture.state_indicator_class not in defaulted_a.get_attribute('class') )
+    vassert( fixture.state_indicator_class in a_with_state.get_attribute('class') )
 
 
 
@@ -93,7 +105,7 @@ class LayoutScenarios(WebFixture):
 
     @scenario
     def stacked_pill_layouts(self):
-        """Using a PillLayout, you can also make MenuItems appear on stacked top of 
+        """Using a PillLayout, you can also make MenuItems appear stacked on top of 
            one another instead of being placed next to one another."""
         self.layout_css_class = {'nav-pills', 'nav-stacked'}
         self.layout = PillLayout(stacked=True)
@@ -107,12 +119,12 @@ class LayoutScenarios(WebFixture):
 
 @test(LayoutScenarios)
 def nav_layouts(fixture):
-    """Navs can be laid out in different ways."""
-    menu = Nav(fixture.view, [])
+    """Navs can be laid out in different ways, and the Layout used creates the html_representation of the Nav"""
+    menu = Nav(fixture.view)
 
-    vassert( not fixture.layout_css_class.issubset(menu.attributes['class'].value) )
+    vassert( not menu.html_representation )
     menu.use_layout(fixture.layout)
-    vassert( fixture.layout_css_class.issubset(menu.attributes['class'].value) )
+    vassert( fixture.layout_css_class.issubset(menu.html_representation.attributes['class'].value) )
 
 
 class DifferentLayoutTypes(WebFixture):
@@ -130,11 +142,11 @@ def justified_items(fixture):
     """Both a PillLayout or TabLayout can be set to make the MenuItems of
        their Nav fill the width of the parent, with the text of each item centered."""
 
-    menu = Nav(fixture.view, []).use_layout(fixture.layout_type())
-    vassert( 'nav-justified' not in menu.get_attribute('class') )
+    menu = Nav(fixture.view).use_layout(fixture.layout_type())
+    vassert( 'nav-justified' not in menu.html_representation.get_attribute('class') )
     
-    menu = Nav(fixture.view, []).use_layout(fixture.layout_type(justified=True))
-    vassert( 'nav-justified' in menu.get_attribute('class') )
+    menu = Nav(fixture.view).use_layout(fixture.layout_type(justified=True))
+    vassert( 'nav-justified' in menu.html_representation.get_attribute('class') )
 
 
 @test(WebFixture)
@@ -147,12 +159,13 @@ def pill_layouts_cannot_mix_justified_and_stacked(fixture):
 
 @test(WebFixture)
 def dropdown_menus(fixture):
-    """You can add one Nav as a dropdown inside another Nav."""
-    menu = Nav(fixture.view, [])
-    sub_menu = Nav(fixture.view, [])
+    """You can add a DropdownMenu as a dropdown inside a Nav."""
+    menu = Nav(fixture.view)
+    sub_menu = DropdownMenu(fixture.view)
+    sub_menu.add_a(A(fixture.view, Url('/an/url'), description='sub menu item'))
     menu.add_dropdown('Dropdown title', sub_menu)
 
-    [item] = menu.children
+    [item] = menu.html_representation.children
     
     vassert( item.tag_name == 'li' )
     vassert( 'dropdown' in item.get_attribute('class') )
@@ -160,23 +173,29 @@ def dropdown_menus(fixture):
     [toggle, added_sub_menu] = item.children
     vassert( 'dropdown-toggle' in toggle.get_attribute('class') )
     vassert( 'dropdown' in toggle.get_attribute('data-toggle') )
+    vassert( '-' in toggle.get_attribute('data-target') )
     vassert( 'caret' in toggle.children[1].get_attribute('class') )
 
     title_text = toggle.children[0].value
     vassert( title_text == 'Dropdown title' )
 
-    vassert( 'dropdown-menu' in added_sub_menu.get_attribute('class').split() )
     vassert( added_sub_menu is sub_menu )
+    vassert( 'dropdown-menu' in added_sub_menu.html_representation.get_attribute('class').split() )
+    vassert( isinstance(added_sub_menu.html_representation, Div) )
 
+    [dropdown_item] = added_sub_menu.html_representation.children
+    vassert( isinstance(dropdown_item, A) )
+    vassert( 'dropdown-item' in dropdown_item.get_attribute('class').split() )
+    
 
 @test(WebFixture)
 def dropdown_menus_can_drop_up(fixture):
     """Dropdown menus can drop upwards instead of downwards."""
-    menu = Nav(fixture.view, [])
-    sub_menu = Nav(fixture.view, [])
+    menu = Nav(fixture.view)
+    sub_menu = Nav(fixture.view)
     menu.add_dropdown('Dropdown title', sub_menu, drop_up=True)
 
-    [item] = menu.children
+    [item] = menu.html_representation.children
     
     vassert( item.tag_name == 'li' )
     vassert( 'dropup' in item.get_attribute('class') )
@@ -185,15 +204,12 @@ def dropdown_menus_can_drop_up(fixture):
 @test(WebFixture)
 def dropdown_menus_right_align(fixture):
     """Dropdown menus can be aligned to the bottom right of their toggle, instead of the default (left)."""
-    menu = Nav(fixture.view, [])
-    defaulted_sub_menu = Nav(fixture.view, [])
-    right_aligned_sub_menu = Nav(fixture.view, [])
 
-    menu.add_dropdown('Dropdown title', defaulted_sub_menu)
-    vassert( 'dropdown-menu-right' not in defaulted_sub_menu.get_attribute('class') )
+    defaulted_sub_menu = DropdownMenu(fixture.view).use_layout(DropdownMenuLayout())
+    vassert( 'dropdown-menu-right' not in defaulted_sub_menu.html_representation.get_attribute('class') )
 
-    menu.add_dropdown('Dropdown title', right_aligned_sub_menu, align_right=True)
-    vassert( 'dropdown-menu-right' in right_aligned_sub_menu.get_attribute('class') )
+    right_aligned_sub_menu = DropdownMenu(fixture.view).use_layout(DropdownMenuLayout(align_right=True))
+    vassert( 'dropdown-menu-right' in right_aligned_sub_menu.html_representation.get_attribute('class') )
 
 
 
