c897340857845f8a69a17047d4d0c2d139e10132
Craig <craig@reahl.org>
2015-12-10 09:08:09 +0200

    resurrect boostrap.ui; add placeholder tests

--- reahl-web/reahl/web/holder/holder.py
+++ reahl-web/reahl/web/holder/holder.py
@@ -18,40 +18,76 @@
 from __future__ import print_function, unicode_literals, absolute_import, division
 import six
 
+from collections import OrderedDict
 
 from reahl.web.fw import Url
 from reahl.web.ui import Img
 
 
-class Theme(object):
-    def __init__(self, bg=None, fg=None, text_size=None, text_font=None, text_align=None):
-        assert text_align in ['left', 'right', None]
-        self.bg = bg
-        self.fg = fg
-        self.text_size = text_size
-        self.text_font = text_font
-        self.text_align = text_align
+class Theme(OrderedDict):
+    """A :class:`Theme` is used as argument to :class:`Placeholder` that influences what the placeholder will look like.
+        Refer: https://github.com/imsky/holder
+    """
+    def __init__(self):
+        super(Theme, self).__init__()
 
-    def as_options(self):
-        options = {}
-        if self.bg is not None:
-            options['bg'] = self.bg
-        if self.fg is not None:
-            options['fg'] = self.fg
-        if self.text_size is not None:
-            options['size'] = str(self.text_size)
-        if self.text_font is not None:
-            options['font'] = self.text_font
-        if self.text_align is not None:
-            options['align'] = self.text_align
-        return options
-        
+
+class PredefinedTheme(Theme):
+    """
+        :param theme_name:
+    """
+    def __init__(self, theme_name):
+        super(PredefinedTheme, self).__init__()
+        self['theme'] = theme_name
+
+
+class CustomTheme(Theme):
+    """
+        :keyword bg:
+        :keyword fg:
+        :keyword text_size:
+        :keyword text_font:
+        :keyword text_align:
+        :keyword outline:
+        :keyword line_wrap:
+    """
+    def __init__(self, bg=None, fg=None, text_size=None, text_font=None, text_align=None, outline=None, line_wrap=None):
+
+        super(CustomTheme, self).__init__()
+        assert text_align in ['left', 'right', None]
+
+        if bg is not None:
+            self['bg'] = bg
+        if fg is not None:
+            self['fg'] = fg
+        if text_size is not None:
+            self['size'] = str(text_size)
+        if text_font is not None:
+            self['font'] = text_font
+        if text_align is not None:
+            self['align'] = text_align
+        if outline is not None:
+            self['outline'] = outline
+        if line_wrap is not None:
+            self['lineWrap'] = str(line_wrap)
+
 
 class PlaceholderImage(Img):
+    """A :class:`PlaceholderImage` renders image placeholders on the client side using SVG.
+        Refer: https://github.com/imsky/holder
+
+        :param x:
+        :param y:
+        :keyword alt:
+        :keyword css_id:
+        :keyword text:
+        :keyword theme:
+    """
     def __init__(self, view, x, y, alt=None, css_id=None, text=None, theme=None):
+
         super(PlaceholderImage, self).__init__(view, alt=alt, css_id=css_id)
         image_url = Url('holder.js/%sx%s' % (x, y))
-        options = (theme or Theme()).as_options()
+        options = theme or Theme()
         if text is not None:
             options['text'] = text
         image_url.set_query_from(options)
