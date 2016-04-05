7584c27c9ec9c9c7104d2c3f1cf0920c1e905af9
Craig <craig@reahl.org>
2016-04-05 09:58:43 +0200

    bootstrap transition: copied reahl-domainui to reahl-domainuib

---
+++ reahl-domainuib/etc/reahl.config.py
@@ -0,0 +1,11 @@
+from __future__ import print_function, unicode_literals, absolute_import, division
+
+from reahl.sqlalchemysupport import SqlAlchemyControl
+
+reahlsystem.root_egg = 'reahl-domainuib'
+reahlsystem.connection_uri = 'postgresql://rhug:rhug@localhost/rhug'
+#reahlsystem.connection_uri = 'sqlite:////tmp/test.db'
+reahlsystem.orm_control = SqlAlchemyControl(echo=False)
+
+
+
