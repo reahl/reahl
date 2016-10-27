ee82edd1b92985d8094c693a20aa07a204054e9c
Craig <craig@reahl.org>
2016-10-27 15:07:11 +0200

    Fixed error in gpg key script; made it more verbose

--- travis/createTestGpgKey.sh
+++ travis/createTestGpgKey.sh
@@ -15,12 +15,13 @@ EOF
 }
 
 function import_gpg_keys () {
-  from_dir = $1
+  from_dir=$1
   gpg --import $from_dir/key.asc $from_dir/key.secret.asc
   gpg --import-ownertrust < $from_dir/trust.asc
 }
 
 if [ "$TRAVIS_SECURE_ENV_VARS" == 'true' ]; then
+  echo "SECRETS are available, fetching reahl GPG signing key"
   pip install awscli
   aws s3 cp s3://$AWS_BUCKET/keys.tgz.enc /tmp/keys.tgz.enc
   openssl aes-256-cbc -K $encrypted_8ad69f44e444_key -iv $encrypted_8ad69f44e444_iv -in /tmp/keys.tgz.enc -out /tmp/keys.tgz -d
@@ -30,5 +31,6 @@ if [ "$TRAVIS_SECURE_ENV_VARS" == 'true' ]; then
   echo "--default-key $GPG_KEY_ID" >> ~/.gnupg/options
   whack_passphrase
 else
+  echo "SECRETS NOT available, using fake key for signing"
   import_gpg_keys travis/keys
 fi
