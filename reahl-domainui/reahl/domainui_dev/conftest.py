f74a6b1dc35e5973f1fd0c1f897767f1cdfaf7f2
Iwan Vosloo <iwan@reahl.org>
2017-03-09 14:18:21 +0200

    Added forgotten file

---
+++ reahl-domainui/reahl/domainui_dev/conftest.py
@@ -0,0 +1,7 @@
+
+# noinspection PyUnresolvedReferences
+from reahl.dev.fixtures import reahl_system_fixture
+
+# noinspection PyUnresolvedReferences
+from reahl.webdev.fixtures import web_server_fixture
+
