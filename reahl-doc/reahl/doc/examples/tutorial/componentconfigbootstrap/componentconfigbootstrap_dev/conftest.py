3d19b66c28d7bea9d0092a86dae0f569d7e3cd4c
Iwan Vosloo <iwan@reahl.org>
2017-03-30 15:31:09 +0200

    Converted ajaxbootstrap and componentconfigbootstrap examples tests to pytest

---
+++ reahl-doc/reahl/doc/examples/tutorial/componentconfigbootstrap/componentconfigbootstrap_dev/conftest.py
@@ -0,0 +1,7 @@
+
+
+from reahl.dev.fixtures import reahl_system_fixture
+
+from reahl.webdev.fixtures import web_server_fixture
+
+
--- reahl-doc/reahl/doc/examples/tutorial/componentconfigbootstrap/componentconfigbootstrap_dev/test_componentconfigbootstrap.py
+++ reahl-doc/reahl/doc/examples/tutorial/componentconfigbootstrap/componentconfigbootstrap_dev/test_componentconfigbootstrap.py
@@ -2,17 +2,24 @@
 from __future__ import print_function, unicode_literals, absolute_import, division
 
 
-from reahl.tofu import test
+from reahl.tofu import Fixture
 
-from reahl.web_dev.fixtures import WebFixture
 from reahl.webdev.tools import Browser, XPath
 
 from reahl.doc.examples.tutorial.componentconfigbootstrap.componentconfigbootstrap import AddressBookUI, Address
 
+from reahl.web_dev.fixtures import web_fixture
+from reahl.sqlalchemysupport_dev.fixtures import sql_alchemy_fixture
+from reahl.domain_dev.fixtures import party_account_fixture
 
-class ConfigFixture(WebFixture):
+
+class ConfigFixture(Fixture):
+    def __init__(self, web_fixture):
+        super(ConfigFixture, self).__init__()
+        self.web_fixture = web_fixture
+
     def new_wsgi_app(self):
-        return super(ConfigFixture, self).new_wsgi_app(site_root=AddressBookUI)
+        return self.web_fixture.new_wsgi_app(site_root=AddressBookUI)
 
     def new_existing_address(self):
         address = Address(name='John Doe', email_address='johndoe@some.org')
@@ -28,11 +35,12 @@ class ConfigFixture(WebFixture):
     def heading_is_displayed(self):
         return self.browser.is_element_present(XPath.heading_with_text(1, 'Addresses'))
 
+config_fixture = ConfigFixture.as_pytest_fixture()
 
-@test(ConfigFixture)
-def add_address(fixture):
+
+def test_add_address(config_fixture):
     """A user can add an address, after which the address is listed."""
-    browser = fixture.browser
+    browser = config_fixture.browser
     
     browser.open('/')
     browser.type(XPath.input_labelled('Name'), 'John')
@@ -40,26 +48,24 @@ def add_address(fixture):
 
     browser.click(XPath.button_labelled('Save'))
     
-    assert fixture.address_is_listed_as('John', 'johndoe@some.org')
+    assert config_fixture.address_is_listed_as('John', 'johndoe@some.org')
 
 
-@test(ConfigFixture)
-def config_was_read_from_file(fixture):
+def test_config_was_read_from_file(web_fixture):
     """The tests are run with config read from an actual config file, not the default config."""
-    assert fixture.context.config.componentconfig.showheader == True
+    assert web_fixture.context.config.componentconfig.showheader == True
 
 
-@test(ConfigFixture)
-def configurable_heading(fixture):
+def test_configurable_heading(web_fixture, config_fixture):
     """Whether the heading is displayed or not, is configurable."""
-    browser = fixture.browser
+    browser = config_fixture.browser
     
-    fixture.context.config.componentconfig.showheader = False
+    web_fixture.context.config.componentconfig.showheader = False
     browser.open('/')
-    assert not fixture.heading_is_displayed()
+    assert not config_fixture.heading_is_displayed()
     
-    fixture.context.config.componentconfig.showheader = True
+    web_fixture.context.config.componentconfig.showheader = True
     browser.open('/')
-    assert fixture.heading_is_displayed()
+    assert config_fixture.heading_is_displayed()
 
 
