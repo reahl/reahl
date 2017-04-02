78680c634a024b295f1e30c62e0a7cd75a04cb74
Iwan Vosloo <iwan@reahl.org>
2017-04-02 18:34:56 +0200

    Reverted tabbedpanel feature example

---
+++ reahl-doc/reahl/doc/examples/features/tabbedpanel/etc/web.config.py
@@ -0,0 +1,6 @@
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+from reahl.doc.examples.features.tabbedpanel.tabbedpanel import TabbedPanelUI
+
+web.site_root = TabbedPanelUI
+
