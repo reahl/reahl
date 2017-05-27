eb2e076a05374fc96bb72343e741a1a0fd74a926
Iwan Vosloo <iwan@reahl.org>
2017-05-27 10:31:16 +0200

    Renamed showHostFingerprints.sh and automatically configure a git hook to prevent comitting as vagrant

--- travis/setupTestGit.sh
+++ travis/setupTestGit.sh
@@ -2,3 +2,19 @@
 
 git config --global user.email $EMAIL
 git config --global user.name "$DEBFULLNAME"
+
+cat > /vagrant/.git/hooks/pre-commit <<EOF
+#!/bin/sh
+
+if [ "$USER" = "vagrant" ] 
+then
+        echo "Please don't commit as the vagrant user...rather commit from your own machine as yourself."
+        exit 1
+else
+        exit 0
+fi
+
+EOF
+
+chmod u+x /vagrant/.git/hooks/pre-commit
+
