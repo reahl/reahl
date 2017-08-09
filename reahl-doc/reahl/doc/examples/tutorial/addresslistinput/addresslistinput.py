95da209ff788bd3488d21a773cc49761aa9c4e6f
Craig Sparks <craig@reahl.org>
2017-08-09 10:09:58 +0200

    add tutorial example with Form that builds on addresslistdb

---
+++ reahl-doc/reahl/doc/examples/tutorial/addresslistinput/addresslistinput.py
@@ -0,0 +1,80 @@
+from __future__ import print_function, unicode_literals, absolute_import, division
+
+from reahl.web.fw import UserInterface, Widget
+from reahl.web.bootstrap.ui import HTML5Page, TextNode, Div, H, P
+from reahl.web.bootstrap.navbar import Navbar, ResponsiveLayout
+from reahl.web.bootstrap.grid import Container
+from reahl.web.bootstrap.forms import TextInput, Form, FormLayout, ButtonInput, ButtonLayout
+from reahl.component.modelinterface import exposed, Field, EmailField, Action, Event
+from reahl.sqlalchemysupport import Session, Base
+from sqlalchemy import Column, Integer, UnicodeText
+
+
+class AddressBookPage(HTML5Page):
+    def __init__(self, view):
+        super(AddressBookPage, self).__init__(view)
+        self.body.use_layout(Container())
+
+        layout = ResponsiveLayout('md', colour_theme='inverse', bg_scheme='primary', toggle_button_alignment='right')
+        navbar = Navbar(view, css_id='my_nav').use_layout(layout)
+        navbar.layout.set_brand_text('Address book')
+        navbar.layout.add(TextNode(view, 'All your addresses in one place'))
+
+        self.body.add_child(navbar)
+        self.body.add_child(AddressBookPanel(view))
+
+
+class AddressForm(Form):
+    def __init__(self, view):
+        super(AddressForm, self).__init__(view, 'address_form')
+        self.use_layout(FormLayout())
+        model_object = Address()
+        self.layout.add_input(TextInput(self, model_object.fields.name, placeholder='...name'))
+        self.layout.add_input(TextInput(self, model_object.fields.email_address, placeholder='email'))
+
+        self.define_event_handler(model_object.events.save)
+        btn = self.add_child(ButtonInput(self, model_object.events.save))
+        btn.use_layout(ButtonLayout(style='primary'))
+
+
+class AddressBookPanel(Div):
+    def __init__(self, view):
+        super(AddressBookPanel, self).__init__(view)
+
+        self.add_child(H(view, 1, text='Addresses'))
+
+        self.add_child(AddressForm(view))
+
+        for address in Session.query(Address).all():
+            self.add_child(AddressBox(view, address))
+
+
+class AddressBox(Widget):
+    def __init__(self, view, address):
+        super(AddressBox, self).__init__(view)
+        self.add_child(P(view, text='%s: %s' % (address.name, address.email_address)))
+
+
+class AddressBookUI(UserInterface):
+    def assemble(self):
+        self.define_view('/', title='Address book', page=AddressBookPage.factory())
+
+
+class Address(Base):
+    __tablename__ = 'addressbook1_address'
+
+    id            = Column(Integer, primary_key=True)
+    email_address = Column(UnicodeText)
+    name          = Column(UnicodeText)
+
+    def save(self):
+        Session.add(self)
+
+    @exposed
+    def fields(self, fields):
+        fields.name = Field(label='Name', required=True)
+        fields.email_address = EmailField(label='Email', required=True)
+
+    @exposed
+    def events(self, events):
+        events.save = Event(label='Add', action=Action(self.save))
