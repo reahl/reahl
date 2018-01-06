9387ec617d69f0ec76bfe1fe87ece8d9bd9947a6
Iwan Vosloo <iwan@reahl.org>
2018-01-06 11:20:36 +0200

    Added mysql connection URI for testing

--- reahl-web/etc/reahl.config.py
+++ reahl-web/etc/reahl.config.py
@@ -3,6 +3,7 @@ from reahl.sqlalchemysupport import SqlAlchemyControl
 
 reahlsystem.root_egg = 'reahl-web'
 reahlsystem.connection_uri = 'postgresql:///reahl'
+#reahlsystem.connection_uri = 'mysql:///reahl'
 #reahlsystem.connection_uri = 'sqlite:////tmp/test.db'
 #reahlsystem.connection_uri = 'sqlite:///:memory:'
 reahlsystem.orm_control = SqlAlchemyControl(echo=False)
