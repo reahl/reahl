ee8ba83c94e620b3e01b9ce305db0973633bee70
Iwan Vosloo <iwan@reahl.org>
2018-04-22 12:27:03 +0200

    Updated copyright notice dates

--- reahl-web/reahl/web_dev/advanced/subresources/test_resourcebasics.py
+++ reahl-web/reahl/web_dev/advanced/subresources/test_resourcebasics.py
@@ -1,4 +1,4 @@
-# Copyright 2013, 2014 Reahl Software Services (Pty) Ltd. All rights reserved.
+# Copyright 2013-2017 Reahl Software Services (Pty) Ltd. All rights reserved.
 #
 #    This file is part of Reahl.
 #
