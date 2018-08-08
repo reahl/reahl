3dc439221d2c79f1bf8fb691f8cf10893863fb85
Craig Sparks <craig@reahl.org>
2018-08-08 17:09:49 +0200

    trying to figure this one out: test_validation

--- reahl-web/reahl/web_dev/inputandvalidation/test_responsive_disclosure.py
+++ reahl-web/reahl/web_dev/inputandvalidation/test_responsive_disclosure.py
@@ -19,12 +19,15 @@ from __future__ import print_function, unicode_literals, absolute_import, divisi
 
 from reahl.tofu import Fixture, expected, scenario, uses
 from reahl.tofu.pytestsupport import with_fixtures
+from reahl.stubble import CallMonitor
 
 from reahl.web_dev.fixtures import WebFixture
 from reahl.webdev.tools import XPath
 from reahl.web.fw import Widget
-from reahl.web.ui import A, Form, Div, SelectInput, Label, P, RadioButtonSelectInput, CheckboxSelectInput, CheckboxInput
-from reahl.component.modelinterface import BooleanField, MultiChoiceField, ChoiceField, Choice, exposed, IntegerField
+from reahl.web.ui import Form, Div, SelectInput, Label, P, RadioButtonSelectInput, CheckboxSelectInput, \
+                         CheckboxInput, ButtonInput, TextInput
+from reahl.component.modelinterface import BooleanField, MultiChoiceField, ChoiceField, Choice, exposed, IntegerField, \
+                                           EmailField, Event, Action, Field
 from reahl.web_dev.inputandvalidation.test_widgetqueryargs import QueryStringFixture
 
 @uses(web_fixture=WebFixture)
@@ -200,7 +203,7 @@ def test_changing_values_do_not_disturb_other_hash_state(web_fixture, query_stri
     assert browser.get_fragment() == '#choice=3&other_var=other_value'
 
 
-def test_inputs_effect_other_parts_of_form():
+def test_inputs_effect_other_parts_of_form(web_fixture):
     """Inputs can trigger refresh of Widgets that contain other inputs in the same form"""
     assert None, 'TODO: relax check_input_placement so that it only breaks if the form does NOT have a specific ID set (it should still break if the form ID is auto-generated' 
     # We need to think carefully about this one.
@@ -208,10 +211,159 @@ def test_inputs_effect_other_parts_of_form():
     # Perhaps the test to change first is test_check_input_placement (inputandvalidation/test_eventhandling.py)
     # ...but I still think we need to have a test here? because this is a requirement relating to responsive disclosure?
 
-def test_validation():
+
+@with_fixtures(WebFixture)
+def test_validation(web_fixture):
     """If a Field is required, but its Input is not currently displayed as part of the form (because of the 
        state of another Input), the form should not expect input for it."""
+
+    fixture = web_fixture
+
+    class CheckModelValue(object):
+        def submit_event(self, model_object):
+            assert model_object.subscribe_to_newsletter == False
+            assert not hasattr(model_object, 'email')
+    check_model_value = CheckModelValue()
+
+    class ModelObject(object):
+        def handle_event(self):
+            check_model_value.submit_event(self)
+
+        @exposed
+        def events(self, events):
+            events.an_event = Event(label='click me', action=Action(self.handle_event))
+
+        @exposed
+        def fields(self, fields):
+            fields.subscribe_to_newsletter = BooleanField(default=False, label='Subscribe to newsletter')
+            fields.email = EmailField(required=True, label='Email')
+
+    class MyForm(Form):
+        def __init__(self, view):
+            super(MyForm, self).__init__(view, 'myform')
+
+            model_object = ModelObject()
+            self.add_child(QuestionSection(self, model_object))
+            self.define_event_handler(model_object.events.an_event)
+            self.add_child(ButtonInput(self, model_object.events.an_event))
+
+    class QuestionSection(Div):
+        def __init__(self, form, model_object):
+            self.model_object = model_object
+            super(QuestionSection, self).__init__(form.view, css_id='questionid')
+
+            checkbox_input = CheckboxInput(form, model_object.fields.subscribe_to_newsletter)
+            self.add_child(Label(form.view, for_input=checkbox_input))
+            checkbox_input.set_id('triggerid')
+            self.add_child(checkbox_input)
+
+            self.enable_refresh()
+            checkbox_input.enable_notify_change(self.query_fields.subscribe_to_newsletter)
+
+            if self.model_object.subscribe_to_newsletter:
+                self.add_child(RequiredInfo(form, self.model_object))
+
+        @exposed
+        def query_fields(self, fields):
+            fields.subscribe_to_newsletter = self.model_object.fields.subscribe_to_newsletter
+
+    class RequiredInfo(Div):
+        def __init__(self, form, model_object):
+            super(RequiredInfo, self).__init__(form.view)
+            text_input = TextInput(form, model_object.fields.email)
+            self.add_child(Label(form.view, for_input=text_input))
+            self.add_child(text_input)
+
+
+
+    from reahl.web.bootstrap.forms import TextInput, FieldSet, FormLayout
+    class IDDocument(object):
+
+        @exposed
+        def fields(self, fields):
+            fields.subscribe_to_newsletter = BooleanField(default=False, label='Subscribe to newsletter')
+
+            types = [Choice('passport', Field(label='Passport')),
+                     Choice('rsa_id', Field(label='RSA ID'))]
+            fields.document_type = ChoiceField(types, label='Type', default='rsa_id', required=False)
+            fields.id_number     = Field(label='ID number', required=True)
+            fields.passport_number = Field(label='Passport number', required=True)
+            countries = [Choice('Ghana', Field(label='Ghana')),
+                         Choice('South Africa', Field(label='South Africa')),
+                         Choice('Nigeria', Field(label='Nigeria')),
+                         Choice('Namibia', Field(label='Namibia')),
+                         Choice('Zimbabwe', Field(label='Zimbabwe')),
+                         Choice('Kenya', Field(label='Kenya'))
+                         ]
+            fields.country = ChoiceField(countries, label='Country', required=True)
+
+
+
+    class IDForm(Form):
+        def __init__(self, view):
+            super(IDForm, self).__init__(view, 'myidform')
+
+            new_document = IDDocument()
+            grouped_inputs = self.add_child(FieldSet(view, legend_text='Identifying document'))
+            grouped_inputs.use_layout(FormLayout())
+            trigger = grouped_inputs.layout.add_input(SelectInput(self, new_document.fields.document_type))
+            grouped_inputs.add_child(IDInputsSection(self, new_document, trigger))
+
+
+    class IDInputsSection(Div):
+        def __init__(self, form, document, trigger_input):
+            self.document = document
+            super(IDInputsSection, self).__init__(form.view, css_id='idinputs')
+            self.enable_refresh()
+            trigger_input.enable_notify_change(self.query_fields.document_type)
+
+            if self.document.document_type == 'passport':
+                self.add_child(PassportInputs(form, self.document))
+            elif self.document.document_type     == 'rsa_id':
+                self.add_child(IDInputs(form, self.document))
+
+        @exposed
+        def query_fields(self, fields):
+            fields.document_type = self.document.fields.document_type
+
+    class PassportInputs(Div):
+        def __init__(self, form, document):
+            super(PassportInputs, self).__init__(form.view)
+            self.use_layout(FormLayout())
+            self.layout.add_input(SelectInput(form, document.fields.country))
+            self.layout.add_input(TextInput(form, document.fields.passport_number))
+
+    class IDInputs(Div):
+        def __init__(self, form, document):
+            super(IDInputs, self).__init__(form.view)
+            self.use_layout(FormLayout())
+            self.layout.add_input(TextInput(form, document.fields.id_number))
+
+    class BigWidget(Widget):
+        def __init__(self, view):
+            super(BigWidget, self).__init__(view)
+            self.add_child(MyForm(view))
+            self.add_child(IDForm(view))
+
+
+    wsgi_app = web_fixture.new_wsgi_app(enable_js=True, child_factory=BigWidget.factory())
+    web_fixture.reahl_server.set_app(wsgi_app)
+    browser = web_fixture.driver_browser
+    browser.open('/')
+
+    web_fixture.pdb()
+
+    assert browser.is_element_present(XPath.input_labelled('Email'))
+    browser.click(XPath.input_labelled('Subscribe to newsletter'))
+    assert not browser.is_element_present(XPath.input_labelled('Email'))
+
+    with CallMonitor(check_model_value.submit_event) as monitor:
+        assert monitor.times_called == 0
+        browser.click(XPath.button_labelled('click me'))
+        assert monitor.times_called == 1
+
     assert None, 'This might just already work, but I think we need a test'
+
 
 def test_input_values_are_retained():
     """When input is intered into an input which is not always displayed, that previously entered input should be 
