031efcf88151843be6a49a4f1c5c80c018a3ff11
Iwan Vosloo <iwan@reahl.org>
2020-11-24 08:23:57 +0200

    Cleaned up dynamiccontent feature example

--- reahl-doc/reahl/doc/examples/features/dynamiccontent/dynamiccontent.py
+++ reahl-doc/reahl/doc/examples/features/dynamiccontent/dynamiccontent.py
@@ -11,7 +11,7 @@ from reahl.web.bootstrap.grid import Container
 
 
 @session_scoped
-class DynamicContentSelect(Base):
+class Calculator(Base):
     __tablename__ = 'dynamic_content_select'
 
     id              = Column(Integer, primary_key=True)
@@ -21,26 +21,21 @@ class DynamicContentSelect(Base):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.select = False
         self.operandA = 1
         self.operandB = 4
-        self.sum = 5
+        self.recalculate()
 
     @exposed
     def fields(self, fields):
-        fields.operandA = ChoiceField([Choice(1, IntegerField(label='1')),
-                                         Choice(2, IntegerField(label='2')),
-                                         Choice(3, IntegerField(label='3'))],
-                                        label='A')
+        fields.operandA = IntegerField(label='A')
         fields.operandB = ChoiceField([Choice(4, IntegerField(label='4')),
-                                         Choice(5, IntegerField(label='5')),
-                                         Choice(6, IntegerField(label='6'))],
-                                        label='B')
-        fields.sum = IntegerField(label='Sum')
+                                       Choice(5, IntegerField(label='5')),
+                                       Choice(6, IntegerField(label='6'))],
+                                      label='B')
 
     @exposed
     def events(self, events):
-        events.something_changed = Event(action=Action(self.recalculate))
+        events.input_changed = Event(action=Action(self.recalculate))
 
     def recalculate(self):
         self.sum = self.operandA + self.operandB
@@ -50,16 +45,16 @@ class DynamicContentForm(Form):
     def __init__(self, view):
         super().__init__(view, 'my_cool_form')
 
-        model_object = DynamicContentSelect.for_current_session()
-        self.enable_refresh(on_refresh=model_object.events.something_changed)
+        calculator = Calculator.for_current_session()
+        self.enable_refresh(on_refresh=calculator.events.input_changed)
 
         grouped_inputs = self.add_child(FieldSet(view, legend_text='Calculator'))
         grouped_inputs.use_layout(InlineFormLayout())
 
-        grouped_inputs.layout.add_input(TextInput(self, model_object.fields.operandA, refresh_widget=self)).use_layout(MarginLayout(2, right=True))
-        grouped_inputs.layout.add_input(SelectInput(self, model_object.fields.operandB, refresh_widget=self))
+        grouped_inputs.layout.add_input(TextInput(self, calculator.fields.operandA, refresh_widget=self)).use_layout(MarginLayout(2, right=True))
+        grouped_inputs.layout.add_input(SelectInput(self, calculator.fields.operandB, refresh_widget=self))
 
-        self.add_child(P(self.view, text='Sum: %s' % model_object.sum))
+        self.add_child(P(self.view, text='Sum: %s' % calculator.sum))
 
 
 class DynamicContentPage(HTML5Page):
