ccd1feecbb5a57b487b7e7180eb55333f36c04ca
Craig Sparks <craig@reahl.org>
2021-04-15 13:44:33 +0200

    test build

--- scripts/createVenv.sh
+++ scripts/createVenv.sh
@@ -2,6 +2,5 @@
 
 VENV_NAME=$1
 
-bash -c "export WORKON_HOME=~/.venvs; . /usr/share/virtualenvwrapper/virtualenvwrapper.sh; mkvirtualenv -p $(which python3.8) $VENV_NAME"
-
+bash -c "export WORKON_HOME=~/.venvs; export VIRTUALENVWRAPPER_PYTHON=$(which python3); . /usr/share/virtualenvwrapper/virtualenvwrapper.sh; mkvirtualenv -p $(which python3) $VENV_NAME"
 
