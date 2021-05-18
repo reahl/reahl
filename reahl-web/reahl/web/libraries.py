c17cf948502d6c16acc054eee031eabbb9718e9d
Craig Sparks <craig@reahl.org>
2021-05-18 16:30:44 +0200

    newer version of bootstrap 4.5.3

--- reahl-web/reahl/web/libraries.py
+++ reahl-web/reahl/web/libraries.py
@@ -267,20 +267,20 @@ class Holder(Library):
 
 
 class Bootstrap4(Library):
-    """Version 4.5.2 of `Bootstrap <http://getbootstrap.com/>`_.
+    """Version 4.5.3 of `Bootstrap <http://getbootstrap.com/>`_.
     """
     def __init__(self):
         super().__init__('bootstrap4')
         self.shipped_in_directory = 'reahl/web/static'
         self.files = [
-                      'bootstrap-4.5.2/css/bootstrap.css',
-                      'bootstrap-4.5.2/css/reahl-patch.css',
-                      'bootstrap-4.5.2/css/bootstrap.css.map',
-                      # 'bootstrap-4.5.2/css/bootstrap-grid.css',
-                      # 'bootstrap-4.5.2/css/bootstrap-grid.css.map',
-                      # 'bootstrap-4.5.2/css/bootstrap-reboot.css',
-                      # 'bootstrap-4.5.2/css/bootstrap-reboot.css.map',
-                      'bootstrap-4.5.2/js/bootstrap.js'
+                      'bootstrap-4.5.3/css/bootstrap.css',
+                      'bootstrap-4.5.3/css/reahl-patch.css',
+                      'bootstrap-4.5.3/css/bootstrap.css.map',
+                      # 'bootstrap-4.5.3/css/bootstrap-grid.css',
+                      # 'bootstrap-4.5.3/css/bootstrap-grid.css.map',
+                      # 'bootstrap-4.5.3/css/bootstrap-reboot.css',
+                      # 'bootstrap-4.5.3/css/bootstrap-reboot.css.map',
+                      'bootstrap-4.5.3/js/bootstrap.js'
                       ]
 
 
