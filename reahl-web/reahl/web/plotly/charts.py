c6685eef9c5ea79abdabc25ff08487999e8f857e
Craig Sparks <craig@reahl.org>
2021-07-06 18:21:55 +0200

    WIP: getting chart to show on ajax event

--- reahl-web/reahl/web/plotly/charts.py
+++ reahl-web/reahl/web/plotly/charts.py
@@ -1,15 +1,22 @@
-from reahl.web.ui import HTMLWidget, Div, LiteralHTML
+from reahl.web.ui import HTMLWidget, Div, LiteralHTML, HTMLElement
 from reahl.web.libraries import PlotlyJS
 from reahl.component.context import ExecutionContext
+
 
 class Chart(HTMLWidget):
     def __init__(self, view, figure, read_check=None, write_check=None):
         super().__init__(view, read_check=read_check, write_check=write_check)
-        ExecutionContext.get_context().config.web.frontend_libraries.add(PlotlyJS(True))
+        #ExecutionContext.get_context().config.web.frontend_libraries.add(PlotlyJS(True))
+        script_html = PlotlyJS(True).header_only_material(None)
+        self.script_coactive_widget = view.add_out_of_bound_widget(LiteralHTML(view, script_html))
 
         containing_div = self.add_child(Div(view))
         self.set_html_representation(containing_div)
         containing_div.add_child(LiteralHTML(view, figure,
 #                                             transform=(lambda i: i.to_html(include_plotlyjs='/static/%s' % PlotlyJS.javascript_filename, full_html=False))))
                                              transform=(lambda i: i.to_html(include_plotlyjs=False, full_html=False))))
+
+    @property
+    def coactive_widgets(self):
+        return super().coactive_widgets + [self.script_coactive_widget]
 
--- reahl-web/reahl/web_dev/test_plotly.py
+++ reahl-web/reahl/web_dev/test_plotly.py
@@ -20,7 +20,8 @@ from reahl.tofu import scenario, Fixture, uses, expected
 from reahl.tofu.pytestsupport import with_fixtures
 from reahl.browsertools.browsertools import XPath, Browser
 from reahl.web_dev.fixtures import WebFixture
-from reahl.web.ui import Div
+from reahl.web.ui import Div, Form, FormLayout, SelectInput, exposed, ChoiceField, Choice
+from reahl.component.modelinterface import IntegerField
 from reahl.web.plotly.charts import Chart
 
 import plotly.graph_objects as go
@@ -86,4 +87,53 @@ def test_no_javascript_when_no_chart_on_page(web_fixture):
 
     plotly_js = XPath('//script[contains(@src,"plotly")]')
     assert browser.get_xpath_count(plotly_js) == 0
+
+
+@with_fixtures(WebFixture)
+def test_adding_chart_with_ajax(web_fixture):
+    """ """
+
+    fixture = web_fixture
+
+    class MyForm(Form):
+        def __init__(self, view):
+            self.choice = 1
+            super().__init__(view, 'my_form')
+
+            self.enable_refresh()
+
+            self.use_layout(FormLayout())
+            self.layout.add_input(SelectInput(self, self.fields.choice, refresh_widget=self))
+
+            if self.choice == 2:
+                self.add_child(Chart(view, go.Figure()))
+
+        @exposed
+        def fields(self, fields):
+            fields.choice = ChoiceField([Choice(1, IntegerField(label='Hide chart')),
+                                         Choice(2, IntegerField(label='Show chart'))],
+                                        label='Choice')
+
+
+    wsgi_app = web_fixture.new_wsgi_app(child_factory=MyForm.factory(), enable_js=True)
+    web_fixture.reahl_server.set_app(wsgi_app)
+    browser = web_fixture.driver_browser
+    browser.open('/')
+
+    # import pdb;pdb.set_trace()
+    # fixture.reahl_server.serve_until(lambda: False)
+
+    #Case: no chart
+    chart = XPath.div().including_class('plotly-graph-div js-plotly-plot')
+    plotly_js = XPath('//script[contains(@src,"plotly")]')
+    assert not browser.is_element_present(chart)
+    assert browser.get_xpath_count(plotly_js) == 0
+
+    select_input = XPath.select_labelled('Choice')
+    browser.select(select_input, 'Show chart')
+
+
+    #Case: chart appears
+    assert browser.is_element_present(chart)
+    assert browser.get_xpath_count(plotly_js) == 1
 
