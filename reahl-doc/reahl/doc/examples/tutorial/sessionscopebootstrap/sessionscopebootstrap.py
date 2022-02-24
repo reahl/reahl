5a2412d4785ae79094991ff6b4a6842fd00047fc
Craig Sparks <craig@reahl.org>
2022-02-24 14:57:12 +0200

    find flipper info

--- reahl-doc/reahl/doc/examples/tutorial/sessionscopebootstrap/sessionscopebootstrap.py
+++ reahl-doc/reahl/doc/examples/tutorial/sessionscopebootstrap/sessionscopebootstrap.py
@@ -60,7 +60,7 @@ class LoginSession(Base):
         user = matching_users.one()
         if user.matches_password(self.password):
             self.current_user = user
-            Session.refresh(self.current_user)
+            self.current_user and self.current_user.name
         else:
             print('User NOT matched password (%s) and password (%s)' % (self.email_address, self.password), flush=False)
             raise InvalidPassword()
