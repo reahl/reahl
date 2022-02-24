9b64f829241f16877d0d05ffa79434bd2a491b14
Craig Sparks <craig@reahl.org>
2022-02-24 15:38:12 +0200

    Flipper related to relationship parked - workaround: access attribute solves it.

--- reahl-browsertools/reahl/browsertools/browsertools.py
+++ reahl-browsertools/reahl/browsertools/browsertools.py
@@ -53,9 +53,9 @@ def patch_Field():
 
 class BasicBrowser:
 
-    def view_source(self, flush=True):
+    def view_source(self):
         for line in html.tostring(self.lxml_html, pretty_print=True, encoding='unicode').split('\n'): 
-            print(line, flush=flush)
+            print(line, flush=True)
 
     def save_source(self, filename):
         with io.open(filename, 'w') as html_file:
