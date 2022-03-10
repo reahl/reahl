227991d8d32719964c294d2bd6c74b9d2e61ae65
Iwan Vosloo <iwan@reahl.org>
2022-03-10 10:52:31 +0200

    Image building fixes

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -27,6 +27,7 @@ if [ ! -f /.provisioned ]; then
     if [ ! -z "$BOOTSTRAP_REAHL_SOURCE" ]; then
         $REAHL_SCRIPTS/scripts/installBuildDebs.sh
         su $REAHL_USER -c -- bash -l -c "
+           . .profile
            cd $BOOTSTRAP_REAHL_SOURCE
            $VENV/bin/python scripts/bootstrap.py --script-dependencies
            $VENV/bin/python scripts/bootstrap.py --pip-installs
