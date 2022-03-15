3d52ce98647c4a09811cda31a453da6cc3c95c35
Iwan Vosloo <iwan@reahl.org>
2022-03-15 08:10:10 +0200

    Create REAHLWORKSPACE if it does not exist during bootstrap

--- reahl-dev/reahl/dev/devdomain.py
+++ reahl-dev/reahl/dev/devdomain.py
@@ -24,6 +24,7 @@ import os.path
 import shutil
 import subprocess
 import logging
+import pathlib
 import email.utils
 from contextlib import contextmanager
 import datetime
@@ -2194,6 +2195,7 @@ class ProjectList(list):
                         dirs[:] = []  # This prunes the tree so it does not walk deeper in here
 
     def save(self, filename):
+        pathlib.Path(pathlib.Path(filename).parent).mkdir(parents=True, exist_ok=True)
         f = open(filename, 'w')
         f.writelines(['%s\n' % i.relative_directory for i in self])
         f.close()
