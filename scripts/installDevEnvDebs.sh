ebe96d2c5535ef8e39c1f47b6dd914c7ebdf7ca8
Craig Sparks <craig@reahl.org>
2022-12-13 10:58:11 +0200

    fix for docker image: install firefox from ppa, as default firefox apt package is wrapper that prompts you to install it using snap.

--- scripts/installDevEnvDebs.sh
+++ scripts/installDevEnvDebs.sh
@@ -3,7 +3,7 @@
 MYSQL="default-libmysqlclient-dev mysql-client"
 POSTGRES="postgresql-client"
 SQLITE="sqlite3 libsqlite3-0"
-DEV_ENV="virtualenvwrapper openssh-server openssh-client xpra python3-paramiko x11-utils firefox screen unzip git apt-utils plantuml graphviz vim less"
+DEV_ENV="virtualenvwrapper openssh-server openssh-client xpra python3-paramiko x11-utils screen unzip git apt-utils plantuml graphviz vim less"
 
 export DEBIAN_FRONTEND=noninteractive
 apt-get update 
@@ -21,6 +21,19 @@ fi
 apt-get install --no-install-recommends -y $DEV_ENV $MYSQL $POSTGRES $SQLITE
 apt-get clean
 rm -rf /var/cache/apt/*
+
+#install firefox manully. Snce ubuntu 22.04 it wants to be installed via snap, which we want to avoid.
+#https://www.omgubuntu.co.uk/2022/04/how-to-install-firefox-deb-apt-ubuntu-22-04
+apt-get install --no-install-recommends -y software-properties-common
+add-apt-repository ppa:mozillateam/ppa
+echo '
+Package: *
+Pin: release o=LP-PPA-mozillateam
+Pin-Priority: 1001
+' > /etc/apt/preferences.d/mozilla-firefox
+echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codename}";' > /etc/apt/apt.conf.d/51unattended-upgrades-firefox
+apt-get update
+apt install  --no-install-recommends -y firefox
 
 #fix for geckodriver not included in ubuntu 22.04 repo (firefox-geckodriver). https://bugs.launchpad.net/ubuntu/+source/firefox/+bug/1968266
 wget https://github.com/mozilla/geckodriver/releases/download/v0.32.0/geckodriver-v0.32.0-linux64.tar.gz
