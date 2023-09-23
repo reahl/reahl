798a57fc8e05d5e808693810cdcdf30310d0f4bf
Craig Sparks <craig@reahl.org>
2023-09-23 10:43:28 +0200

    add checkout test to verify packaging

--- reahl-doc/reahl/doc_dev/checkout.sh
+++ reahl-doc/reahl/doc_dev/checkout.sh
@@ -38,7 +38,42 @@ fi
 cd -
 rm -rf /tmp/migrationexamplebootstrap
 
+check_existence() {
+    local error=0
+
+    if [ $# -eq 0 ]; then
+        echo "No arguments provided."
+        return 1
+    fi
+
+    for path in "$@"; do
+        if [ ! -e "$path" ]; then
+            echo "Does not exist: $path"
+            error=1
+        fi
+    done
+
+    return $error
+}
+
+#verify some packaging niggles
+cd /tmp; reahl example -f howtos.hellodockernginx
+cd -
+expected_directories=("/tmp/hellodockernginx/prod/etc" "/tmp/hellodockernginx/etc" "/tmp/hellodockernginx/scripts")
+expected_files=("/tmp/hellodockernginx/prod/nginx/Dockerfile")
+check_existence "${expected_directories[@]}" "${expected_files[@]}"
+error_code=$?
+
+if [ $error_code -eq 0 ]
+then
+    results["hellodockernginx (packaging)"]="."
+else
+    results["hellodockernginx (packaging)"]="E"
+fi
+
+#Disply the results of all the checks
 for x in "${!results[@]}"
 do
     echo ${results["$x"]} $x
 done
+
