b5c5c8232ba5bec95d6731b783ce875b57a83dd5
Iwan Vosloo <iwan@reahl.org>
2023-10-26 15:10:29 +0200

    Updated to use SqlAlchemy 2; issues remain

--- reahl-sqlalchemysupport/reahl/sqlalchemysupport/sqlalchemysupport.py
+++ reahl-sqlalchemysupport/reahl/sqlalchemysupport/sqlalchemysupport.py
@@ -27,8 +27,7 @@ import logging
 import pprint
 
 from sqlalchemy import *
-from sqlalchemy.orm import sessionmaker, scoped_session, relationship
-from sqlalchemy.ext.declarative import instrument_declarative, declarative_base, DeclarativeMeta
+from sqlalchemy.orm import sessionmaker, scoped_session, relationship, DeclarativeBase, DeclarativeMeta, declarative_base
 from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy import Column, Integer, ForeignKey
 from alembic.runtime.migration import MigrationContext
@@ -105,14 +104,17 @@ def ix_name(table_name, column_name):
 Session = scoped_session(sessionmaker(autoflush=True, autocommit=False), scopefunc=reahl_scope) #: A shared SQLAlchemy session, scoped using the current :class:`reahl.component.context.ExecutionContext`
 metadata = MetaData(naming_convention=naming_convention)  #: a metadata for use with other SqlAlchemy tables, shared with declarative classes using Base
 
-
 class DeclarativeABCMeta(DeclarativeMeta, ABCMeta):
     """ Prevent metaclass conflict in subclasses that want multiply inherit from
     ancestors that have ABCMeta as metaclass """
     pass
 
+#class Base(DeclarativeBase, metaclass=DeclarativeABCMeta):
+#    """A Base for using with declarative."""
+#    __abstract__ = True
+#    metadata = metadata
 
-Base = declarative_base(class_registry=weakref.WeakValueDictionary(), metadata=metadata, metaclass=DeclarativeABCMeta)    #: A Base for using with declarative
+Base = declarative_base(class_registry=weakref.WeakValueDictionary(), metadata=metadata, metaclass=DeclarativeABCMeta)    
 
 
 class QueryAsSequence:
@@ -274,7 +276,6 @@ class SqlAlchemyControl(ORMControl):
         self.engine = create_engine(config.reahlsystem.connection_uri, **create_args)
         self.engine.echo = self.echo
         self.engine.connect()
-        metadata.bind = self.engine
         Session.configure(bind=self.engine)
 
         self.instrument_classes_for(config.reahlsystem.root_egg)
@@ -300,14 +301,17 @@ class SqlAlchemyControl(ORMControl):
 
     @property
     def connected(self):
-        return metadata.bind is not None
+        return self.engine
 
     def get_or_initiate_transaction(self):
         assert self.connected
-        return Session().transaction
+        if Session().in_transaction():
+            return Session().get_transaction()
+        return Session.begin()
     
     def finalise_session(self):
-        nested = Session().transaction.nested
+        transaction = Session().get_transaction()
+        nested = transaction and transaction.nested
         if nested:   
             # This is necessary to facilitate testing.  When testing, code continually
             # runs in a nested transaction inside a real transaction which will both
@@ -315,22 +319,22 @@ class SqlAlchemyControl(ORMControl):
             # nested state of the transaction to detect that it is called during a test.
             # If called during a test, this method should NOT commit, and it should NOT
             # nuke the session
-            Session.flush()
+            Session().flush()
             return
 
         self.commit()
 
         context = ExecutionContext.get_context()
         if context.system_control.db_control.is_in_memory:
-            Session.expunge_all()
+            Session().expunge_all()
         else:
             Session.remove()
         
     def disconnect(self):
         """Disposes the current SQLAlchemy Engine and .remove() the Session."""
         assert self.connected
-        metadata.bind.dispose()
-        metadata.bind = None
+        self.engine.dispose()
+        self.engine = None
         Session.remove()
 
     def commit(self):
