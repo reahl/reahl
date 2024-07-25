5041f55b55f93febff7f855d9e36572fc32166fc
Iwan Vosloo <iwan@reahl.org>
2024-07-25 10:42:22 +0200

    Removed debug code

--- reahl-component/reahl/component/context.py
+++ reahl-component/reahl/component/context.py
@@ -112,7 +112,6 @@ class ExecutionContext:
         session = getattr(self, 'session', None)
         if not session:
             return 'en_gb'
-        return self.session.get_interface_locale()
         try:
             return self.session.get_interface_locale()
         except Exception as ex:
